7438f007f40e0e6b8fb548eb6baa49dc85a330cf
Koki Wataoka <contact@wataoka.com>
2018-06-20 22:59:58 +0900

    first commit

---
+++ modeler.py
@@ -0,0 +1,80 @@
+"""
+Modeler
+
+Author: Wataoka Koki
+E-mail: koki.wataoka@st.osaka-cu.ac.jp
+"""
+
+import os
+from collections import OrderedDict
+
+class Modeler():
+
+    """
+    modelerの使い方
+
+    1. modelerオブジェクトを生成する。
+    2. add関数でmodel達を追加する。(modelはコンパイル済み)
+    3. start関数でmodel達を一つずつ学習, 評価, 保存を繰り返す。
+    """
+
+    def __init__(self):
+        """
+        modelsディクショナリについて
+        key: model (modelオブジェクト)
+        value: acc (float)
+        """
+        self.models = {}
+
+
+    def add(self, model):
+        self.models[model] = 0.0
+
+
+    def start(self, data, batch_size=128, epochs=500, verbose=1):
+
+        """
+        モデル達を一つずつ学習させる。
+
+        Args:
+            data: (x_train, y_train), (x_test, y_test)という構造のタプル
+            batch_size: バッチサイズ, int
+            epochs: エポックの回数, int
+            verbose: 途中出力の設定(1がおすすめ), int
+        Return:
+            無し
+        """
+
+        for model in self.models:
+            model.summary()
+            model.fit(x_train, y_train,
+                      batch_size=batch_size,
+                      epochs=epochs,
+                      verbose=verbose)
+            self.models[model] = model.evaluate(x_test, y_test)[0]
+
+
+
+
+    def save(self, models_dir="./models/", n_save=1):
+
+        """
+        モデルオブジェクト達を保存する。上位から順に保存される。
+
+        Args:
+            models_dir: 保存するディレクトリ, string
+            n_save: 保存するモデルの数, int
+        Return:
+            無し
+        """
+
+        models = OrderedDict(sorted(self.models.items(), key=lambda x:x[1], reverse=True))
+
+        if not os.path.exists(models_dir):
+            os.mkdir(models_dir)
+
+        for n, model in enumerate(models):
+            if n < n_save:
+                model.save(models_dir + "model_" + str(n) + ".h5")
+            else:
+                break
---
+++ sample.py
@@ -0,0 +1,61 @@
+from keras.datasets import mnist
+from keras.utils.np_utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense
+
+from modeler import Modeler
+
+def load_data():
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    x_train = x_train.reshape(60000, 784)
+    x_test = x_test.reshape(10000, 784)
+    x_train = x_train.astype('float32') / 255
+    x_test = x_test.astype('float32') / 255
+    y_train = to_categorical(y_train, 10)
+    y_test = to_categorical(y_test, 10)
+
+    return (x_train, y_train), (x_test, y_test)
+
+
+def generate_models():
+    model1 = Sequential()
+    model1.add(Dense(64, activation='relu', input_dim=784))
+    model1.add(Dense(10, activation='softmax'))
+    model1.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model2 = Sequential()
+    model2.add(Dense(128, activation='relu', input_dim=784))
+    model2.add(Dense(10, activation='softmax'))
+    model2.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model3 = Sequential()
+    model3.add(Dense(256, activation='relu', input_dim=784))
+    model3.add(Dense(10, activation='softmax'))
+    model3.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    return (model1, model2, model3)
+
+
+
+if __name__ == "__main__":
+
+    data = load_data()
+
+    (model1, model2, model3)  = generate_models()
+
+    modeler = Modeler()
+    modeler.add(model1)
+    modeler.add(model2)
+    modeler.add(model3)
+    modeler.start(x_train, y_train,
+                  x_test, y_test,
+                  epochs=1)
+    modeler.save()
